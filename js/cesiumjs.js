2762cad59d856e8f1b0bfbadea553c10e0c0b6b5
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-01-19 18:18:27 +0100

    Change1
    More changes

--- js/cesiumjs.js
+++ js/cesiumjs.js
@@ -1,16 +1,17 @@
+	var latitude = -2.002, longitude = 43.314, tile;
+	var meshPosX = 0;
 	var aCesiumTerrainProvider = new Cesium.CesiumTerrainProvider({
 		url : '//cesiumjs.org/stk-terrain/tilesets/world/tiles'
 	});
+	
 	function loadedTerrainProvider(){
-		var latitude, longitude, cesium;
-		latitude = -2,002;
-		longitude = 43.314;
 		var positionLonLat = Cesium.Cartographic.fromDegrees(longitude, latitude);
 		positionTileXY = aCesiumTerrainProvider.tilingScheme.positionToTileXY(positionLonLat,12);		
-		console.log(getTile(positionTileXY));
+		tile = getTile(positionTileXY);
 		aCesiumTerrainProvider.requestTileGeometry(positionTileXY.x,positionTileXY.y,12,true).then(function(data){
 			mesh1(data);
 		});
+		longitude = longitude + tile.distanceCoordinate;
 	}
 	
 	function getTile(positionTileXY){
@@ -31,7 +32,8 @@
 		this.southeast = southeast;
 		this.northwest = northwest;
 		this.southwest = southwest;
-		this.distance = ((northeast.latitude - southeast.latitude)/0.01)*1.1132;
+		this.distanceKm = ((northeast.latitude - southeast.latitude)/0.01)*1.1132;
+		this.distanceCoordinate = northeast.latitude - southeast.latitude;
 	}
 	/*
 		CLASE COORDENADA
@@ -79,32 +81,15 @@
 		for(var i=0; i < heights.length; i++){
 			geometry.vertices.push( new THREE.Vector3(Math.round(x[i]/1000),Math.round(y[i]/1000),Math.round(heights[i]/1000)));
 		}
-
 		for(var i=0; i < facesQuantized.length; i=i+3){
 			geometry.faces.push(new THREE.Face3(facesQuantized[i], facesQuantized[i+1], facesQuantized[i+2]));
 			//console.log(facesQuantized[i] +","+facesQuantized[i+1]+","+ facesQuantized[i+2]);
 		}
-		
 		//geometry.computeBoundingSphere();
 		mesh = new THREE.Mesh( geometry, new THREE.MeshNormalMaterial( {wireframe : true}) );
-		mesh.rotation.x =  Math.PI / 180 * (-90);
+		//mesh.rotation.x =  Math.PI / 180 * (-90);
+		mesh.position.set(meshPosX, 0, 0);
 		scene.add(mesh);
 	}
-	/*
-		FUNCIÓN OBTENCION DATOS CESIUM
-	*/
-	function foo(data) {
-		//PRINCIPAL
-		var cesium = data;
-		mesh1(cesium);	
-	}
-		
-	function changeHeight(min, max, height) {
-		//Llamada de la función : var height = changeHeight(cesium._minimumHeight, cesium._maximumHeight, verticesQuantized[1]);
-		var minQ = 0, maxQ = 32767;
-		var correct;
-			interval = max - min;
-			interval = interval/maxQ;
-			correct = min + (height*interval);
-		return correct;
-	}
+
+
