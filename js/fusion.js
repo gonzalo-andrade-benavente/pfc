a3130dce706527d52571095dd4d36b7dd966edfb
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-01-20 16:02:10 +0100

    PFC Three.js
    More changes.

--- js/fusion.js
+++ js/fusion.js
@@ -1,1 +1,13 @@
 /* Merge geometries in one mesh */
+/*	
+	- Debemos saber la coordenada inicial y la final para saber cuantos tiles usaremos.
+	- Cada arista de un tile tiene un diametro de 4,9km aproximadamente.
+	- Debemos tener un número que al elevarlo al cuadrado nos indique la cantidad 
+	de tiles que debemos obtener.
+
+*/
+var tiles = [];
+function merge(){
+	
+	
+}
