5053da7e50f9194e5297410dd5aeb48623fe776a
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-01-27 16:15:07 +0100

    PFC Three.js
    PFC Union.

---
+++ js/my_cesium.js
@@ -0,0 +1,91 @@
+	/* 
+		PFC my_cesium Library: CesiumJS  
+		This library uses tiles of CesiumJS.
+	*/
+	//variable to access to data in Cesium.
+	var aCesiumTerrainProvider = new Cesium.CesiumTerrainProvider({
+		url : '//cesiumjs.org/stk-terrain/tilesets/world/tiles'
+	});
+	/*
+		Function request data.
+	*/
+	function requestTilesWhenReady() {
+		if (aCesiumTerrainProvider.ready) {
+			console.log("[PFC]:Server Terrain Provider ready");
+			init();
+		} else {
+			//console.log("[PFC]:Waiting a Terrain Provider is ready");
+			setTimeout(requestTilesWhenReady, 10);
+		}
+	}
+	
+	requestTilesWhenReady();
+	
+	function init(){
+		/*
+			I have acces to variables coordinates and map.
+		*/
+		var coor_x, coor_y, coor_z, latitude, longitude;		
+		coor_x = 0; coor_y = 0; coor_z = 0;
+		latitude = coordinates[0][0], longitude = coordinates[0][1];
+		var positionLonLat = Cesium.Cartographic.fromDegrees(longitude, latitude);
+		positionTileXY = aCesiumTerrainProvider.tilingScheme.positionToTileXY(positionLonLat,12);
+		tile = getTile(positionTileXY);
+		
+		/*
+			Map Rectangle for the first coordinate, study the rest of coordinates.
+			North-West and South-East
+		*/
+		var bounds = [[tile.northwest.latitude, tile.northwest.longitude], [tile.southeast.latitude, tile.southeast.longitude]];
+		L.rectangle(bounds, {color: "#ffffff", weight: 2, fillOpacity:0 }).addTo(map);
+		//map.setZoom(14);
+		map.setMaxBounds(bounds);
+		
+		aCesiumTerrainProvider.requestTileGeometry(positionTileXY.x,positionTileXY.y,12,true).then(function(data){
+			//mesh(data, x, y, z);
+			createMesh();
+		});
+	}
+	
+	function createMesh() {
+		console.log("createMesh()");
+	}
+	
+	/*
+		CLASS Tile
+	*/
+	function Tile(northeast, southeast, northwest, southwest) {
+		this.northeast = northeast;
+		this.southeast = southeast;
+		this.northwest = northwest;
+		this.southwest = southwest;
+		this.distanceKm = ((northeast.latitude - southeast.latitude)/0.01)*1.1132;
+		this.distanceCoordinate = northeast.latitude - southeast.latitude;
+	}
+	/*
+		CLASS Cordinate
+	*/
+	function Coordinate(latitude, longitude) {
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	/*
+		FUNCTION radians to degrees.
+	*/
+	function radianToDegrees(radians) {
+		return (radians * 180)/ Math.PI;	
+	}
+	/*
+		FUNCTION create a Tile Cesium Rectangle with vertices in degrees[latitude and longitude].
+		sw: SouthWest, se:SouthEast, nw: NorthWest, ne:NorthEast. 
+	*/
+	function getTile(positionTileXY) {
+		var myTile, sw, se, nw, ne;;
+		var rectangleTileXY = aCesiumTerrainProvider.tilingScheme.tileXYToRectangle(positionTileXY.x, positionTileXY.y, 12);
+		sw = new Coordinate(radianToDegrees(Cesium.Rectangle.southwest(rectangleTileXY).latitude),radianToDegrees(Cesium.Rectangle.southwest(rectangleTileXY).longitude));
+		se = new Coordinate(radianToDegrees(Cesium.Rectangle.southeast(rectangleTileXY).latitude),radianToDegrees(Cesium.Rectangle.southeast(rectangleTileXY).longitude));
+		nw = new Coordinate(radianToDegrees(Cesium.Rectangle.northwest(rectangleTileXY).latitude),radianToDegrees(Cesium.Rectangle.northwest(rectangleTileXY).longitude));
+		ne = new Coordinate(radianToDegrees(Cesium.Rectangle.northeast(rectangleTileXY).latitude),radianToDegrees(Cesium.Rectangle.northeast(rectangleTileXY).longitude));
+		myTile = new Tile(ne, se, nw, sw);
+		return myTile;
+	}
