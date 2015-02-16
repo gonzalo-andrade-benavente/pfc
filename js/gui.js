21e1f5fd0c16025c0051b77bb585c001b97e5fe9
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-02-16 16:16:37 +0100

    PFC Three.js
    More changes.

--- js/gui.js
+++ js/gui.js
@@ -34,25 +34,7 @@ function createGUI() {
 				console.log("[PFC gui.js]: combined_geometry doesn't exist.");
 			}
 		}
-		this.clean = function () {
-			if (scene.children.length != 0) {
-				scene.remove(scene.children[0]);
-				this.clean();
-			} else {
-				console.log("[PFC gui.js]: Childrens in scene removed.")
-			}
-		}
-		this.clone = function () {
-			var geometry = scene.children[0].geometry.clone();
-			for(i = 0; i < geometry.vertices.length; i++)
-				geometry.vertices[i].z = geometry.vertices[i].z/2;
-			var material = new THREE.MeshBasicMaterial( { color: "rgb(255,0,0)", wireframe: true ,side:THREE.DoubleSide} );
-			var mesh = new THREE.Mesh(geometry, material);
-			mesh.position.set(scene.children[0].position.x - 10, scene.children[0].position.y, scene.children[0].position.z);
-			mesh.rotation.x =  Math.PI / 180 * (-90);
-			scene.remove(scene.children[0]);
-			scene.add(mesh);
-		}
+
 	}
 	
 	gui = new dat.GUI();
@@ -66,12 +48,9 @@ function createGUI() {
 		for (var i = 0; i < scene.children.length; i++ )
 			scene.children[i].material.wireframe = e;
 	});
-	//gui.add(controls, 'combined').name('Combinado(merge)');
 	gui.add(controls, 'map').name('Mapa');
 	gui.add(controls, 'home').name('Inicio');
 	gui.add(controls, 'combined').name('Visualizar Terreno');
-	//gui.add(controls, 'clean').name('Limpiar escena');
-	//gui.add(controls, 'clone').name('Clonar geometrias');
 	gui.add(controls, 'refresh').name('Actualizar (F5)');
 }
 
