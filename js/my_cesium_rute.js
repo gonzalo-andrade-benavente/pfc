49d42a18cab654f2b4a9516f88313191e20f9092
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-03-02 16:02:01 +0100

    PFC Three.js
    More changes.

--- js/my_cesium_rute.js
+++ js/my_cesium_rute.js
@@ -54,7 +54,7 @@
 	function createMap(coord) {
 		L.mapbox.accessToken = 'pk.eyJ1IjoiZ29uemFsaXRvIiwiYSI6IlVJTGIweFUifQ.waoF7m8PZbBM6u8Tg_rR7A';
 		map = L.mapbox.map('map', id_maps[id_map]);
-		map.scrollWheelZoom.disable();
+		//map.scrollWheelZoom.disable();
 		coordinates = coord;
 		checkTile(coordinates, 14);
 		rectangle_tiles = createRectangle(info_tiles, 14);
@@ -115,12 +115,15 @@
 		var polyline = L.polyline(line_points, polyline_options).addTo(map);
 		//console.log("[PFC my_cesium_rute.js]: Total coordinates "+coordinates.length);
 		var static_image_json;
-		var width = 504, height = 630;
+		var width = 500, height = 700;
 		var json_coordinates;
 		var coordinate_before = true;
 		var fails = 1;
+		
+		map.setZoom(16);
 		if (rectangle_tiles.length > 0) {
 			for(i = 0; i < rectangle_tiles.length ; i++) {
+			fails = 1;
 			coordinate_before = true;
 			//for(i = 0; i < 2; i++) {
 				console.log("[PFC my_cesium_rute.js] Tile " + i);
@@ -146,13 +149,14 @@
 					for(j = rectangle_tiles[i].index; j < coordinates.length; j++){
 						if ((coordinates[j][0] < rectangle_tiles[i].bounds[0][0]) && (coordinates[j][0] > rectangle_tiles[i].bounds[1][0]) && (coordinates[j][1] > rectangle_tiles[i].bounds[0][1]) && (coordinates[j][1] < rectangle_tiles[i].bounds[1][1])) {
 							if (coordinate_before) {
-								//json_coordinates.push(coordinates[j-1]);
+								//Add last of the previous tile.
 								if (j > 0) {
 									json_coordinates.push(coordinates[j-1]);
 									coordinate_before = false;
 								}
 							}
 							json_coordinates.push(coordinates[j]);
+							//console.log(j);
 							/*
 							console.log(j);
 							L.mapbox.featureLayer({
@@ -177,6 +181,24 @@
 							//console.log(coordinates[j]);
 						}
 					}
+					//Add the first of the next tile.
+					var coordinate_after = rectangle_tiles[i].index + json_coordinates.length - 1;
+					if (coordinate_after < coordinates.length) {
+						//Handle strange error.
+						if (json_coordinates[json_coordinates.length-1] == coordinates[coordinate_after]) 
+							coordinate_after++;
+						json_coordinates.push(coordinates[coordinate_after]);
+					}
+					
+						
+					/*
+					if ((rectangle_tiles[i].index + json_coordinates.length - 1) < j) {
+						if (fails == 1)
+							console.log('final' + (rectangle_tiles[i].index + json_coordinates.length + 1));
+						else 
+							console.log('final' + (rectangle_tiles[i].index + json_coordinates.length - 1));
+					}
+					*/
 										
 					//var reverse_line_points = new Array(json_coordinates.length);
 					var reverse_line_points = new Array();
@@ -274,16 +296,16 @@
 			xhr.open("GET", url+"?"+contenido, true);
 			xhr.send();
 			xhr.onreadystatechange = function () {
+				/*
 				if (xhr.readyState == 4 && xhr.status == 200) {
-					/*
 					if (parseInt(xhr.responseText) != rectangle_tiles.length)
 						drawRute();
 					else {
 						console.log("[PFC my_cesium_rute.js]: All textures creates successfully");
-						//window.open("./PFCMyMesh.html", "_self");
+						window.open("./PFCMyMesh.html", "_self");
 					}
-					*/				
 				}
+				*/
 			}
 		}
 	}
