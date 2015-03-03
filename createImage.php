8e7fe6d29b47608035cd4d2030eb6c84acf3d224
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-03-03 11:34:34 +0100

    PFC Three.js
    More changes.

--- createImage.php
+++ createImage.php
@@ -1,32 +1,4 @@
-<?php	
-	/*
-	//resource imagecreate ( int $width , int $height )
-	$background_image = imagecreate($columns*510, $rows*700)
-		or die("Cannot Initialize new GD image stream");
-	$background_color = imagecolorallocate($background_image, 255, 0, 0);
-	$times = 0;
-	$pos_x = 0; $pos_y = 700 * ($rows - 1);
-	for($i = 0; $i < $columns; $i++) {
-		for($j = 0; $j < $rows; $j++) {
-			$source_image = 'textures/'.$file.$times.'.png';
-			//$source_image = 'textures/ruta12.png';
-			$image = imagecreatefrompng($source_image);
-			//$size[0] width, $size[1] height.
-			$size = getimagesize($source_image);			
-			imagecopymerge($background_image, $image, $pos_x, $pos_y, 0, 0, $size[0], $size[1], 100);
-			$pos_y = $pos_y - 700;
-			$times++;
-			imagedestroy($image);
-		}
-		$pos_x = $pos_x + 510;
-		$pos_y = 700 * ($rows - 1);
-	}
-	
-	$resp = imagepng($background_image,'texture/'.date('ymdhis').'.png');
-	*/
-	
-	
-	
+<?php		
 	$rows = (int) $_GET['rows'];
 	$columns = (int) $_GET['columns'];
 	$file = $_GET['name'];
@@ -57,23 +29,4 @@
 	imagepng($background_image,'texture/'.date('ymdhis').'.png');
 	imagedestroy($background_image);
 	echo $source_image;
-	/*
-	$source_image = 'textures/ruta11.png';
-	$source_image1 = 'textures/ruta14.png';
-	$image = imagecreatefrompng($source_image);
-	$size = getimagesize($source_image);
-
-	
-	$image1 = imagecreatefrompng($source_image1);
-	$x = 0;
-	for($i = 0; $i < 2; $i++) {
-		imagecopymerge($background_image, $image, $x, 0, 0, 0, $size[0], $size[1], 100);
-		//imagecopymerge($background_image, $image1, $size[0], 0, 0, 0, $size[0], $size[1], 100);
-		$x = $x + $size[0];
-	}
-	
-	//$resp = imagepng($background_image,'texture/'.date('ymdhis').'.png');
-	imagedestroy($image);
-	imagedestroy($image1);
-	*/
 ?>
