b181aaa65a8c56679b9fb4e60f720ea9e09a98a9
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-03-26 12:04:19 +0100

    PFC Three.js
    More changes, change image format.

--- createImage.php
+++ createImage.php
@@ -14,8 +14,8 @@
 	$columns = (int) $_GET['columns'];
 	$file = $_GET['name'];
 	//510 - 697
-	$width = 509;
-	$heigth = 702;
+	$width = (509 * 0.5) - 1;
+	$heigth = (702 * 0.5) - 1;
 	//$background_image = @imagecreate($width * $columns, $heigth * $rows)
 	//	or die("Cannot Initialize new GD image stream");
 		
@@ -31,6 +31,7 @@
 	$y = $heigth * ($rows - 1);
 	for($i = 0; $i < $columns; $i++) {
 		for($j = 0; $j < $rows; $j++) {
+			/*
 			if($type == 'png') {
 				$source_image = 'textures/'.$file.$times.'.png';
 				$image = imagecreatefrompng($source_image);
@@ -38,6 +39,9 @@
 				$source_image = 'textures/'.$file.$times.'.png';
 				$image = imagecreatefromjpeg($source_image);
 			}
+			*/
+			$source_image = 'textures/'.$file.$times.'.jpeg';
+			$image = imagecreatefromjpeg($source_image);
 			$size = getimagesize($source_image);
 			//imagecopymerge($background_image, $image, $x, $y, 0, 0, $size[0], $size[1], 100);
 			//imagecopy($background_image, $image, $x, $y, 0, 0, $size[0]-10, $size[1]-10);
@@ -49,10 +53,10 @@
 		$x = $x + $width;
 		$y = $heigth * ($rows - 1);
 	}
-	//imagejpeg($background_image,'export/'.$file.'.jpeg');
-	imagepng($background_image,'export/'.$file.'.png');
+	imagejpeg($background_image,'export/'.$file.'.jpeg');
+	//imagepng($background_image,'export/'.$file.'.png');
 	imagedestroy($background_image);
-	
+	/*
 	$change_image = 'export/'.$file.'.png';
 	$new_size = getimagesize($change_image);
 	$nuevo_ancho = $new_size[0] * 0.5;
@@ -64,7 +68,7 @@
 	imagepng($new_image,'export/'.$file.'.png');
 	imagedestroy($new_image);
 	imagedestroy($image);
-	
-	echo 'export/'.$file.'.png';
+	*/
+	echo 'export/'.$file.'.jpeg';
 	
 ?>
--- getTexture.php
+++ getTexture.php
@@ -11,7 +11,17 @@
 	$information = json_decode($_POST['information']);
 	$dev;
 	foreach($information as $tile) {
-		copy( $tile->direction, 'textures/'.$tile->name.'.png');
+		copy( $tile->direction, 'textures/'.$tile->name.'.jpeg');
+		$change_image = 'textures/'.$tile->name.'.jpeg';
+		$new_size = getimagesize($change_image);
+		$nuevo_ancho = $new_size[0] * 0.5;
+		$nuevo_alto = $new_size[1] * 0.5;
+		$new_image = imagecreatetruecolor($nuevo_ancho, $nuevo_alto);
+		$image = imagecreatefromjpeg($change_image);
+		imagecopyresized($new_image, $image, 0, 0, 0, 0, $nuevo_ancho, $nuevo_alto, $new_size[0], $new_size[1]);
+		imagejpeg($new_image,'textures/'.$tile->name.'.jpeg');
+		imagedestroy($new_image);
+		imagedestroy($image);
 	}
 	echo "[PFC getTexture.php]: All textures copy in server.";
 	//echo $information[0]->direction;
