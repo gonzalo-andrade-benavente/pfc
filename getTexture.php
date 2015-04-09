741eaba4586593091f1cf0d21e63d0fc77d60747
gonzalinho007 <gonzalo.andrade@gmail.com>
2015-04-09 13:26:51 +0200

    PFC Three.js
    More changes.

--- getTexture.php
+++ getTexture.php
@@ -1,13 +1,4 @@
 <?php
-	/* Save image mapbox to use like a texture. */
-	/*
-	$direction = str_replace(" ", "+", $_GET["direction"]);
-	copy( $direction, 'textures/'.$_GET["name"].'.png');
-	*/
-	//echo "";
-	//$total_imagenes = count(glob("textures/{*.jpg,*.gif,*.png}",GLOB_BRACE));
-	//echo $total_imagenes;
-	//echo 'textures/'.$_GET["name"].'.png';
 	$information = json_decode($_POST['information']);
 	$dev;
 	foreach($information as $tile) {
@@ -24,5 +15,4 @@
 		imagedestroy($image);
 	}
 	echo "[PFC getTexture.php]: All textures copy in server.";
-	//echo $information[0]->direction;
 ?>
